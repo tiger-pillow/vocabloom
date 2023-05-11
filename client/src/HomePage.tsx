c5e4294c52f4891bd01c33107f8d96508f03fb2e
yiyuan <gladysfangcs@gmail.com>
2023-05-12 00:30:12 +0200

    installed tailwind, started on the words

---
+++ client/src/HomePage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function HomePage() {
+    return (
+        <div className="container m-10 text-lg text-blue-800">
+            HomePage
+        </div>
+    );
+}
---
+++ client/src/WordPage.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const verbs = [
+    "pleuvoir", 
+    "asseoir", 
+    "mourir", 
+    "pleurer", 
+    "accueillir",
+    "aboyer"
+]
+
+const nouns = [
+    "la réticence", 
+    "la pénurie", 
+    "le rendement", 
+    "le procédé", 
+
+]
+
+const adjectives = [
+    "délétère", 
+    "raide", 
+
+]
