fd0a74b7af718759d81deaef9589c12c31d9ebd4
yiyuan <gladysfangcs@gmail.com>
2023-05-13 00:53:21 +0200

    added boxes for letters, bug with event handler

--- client/src/WordPage.tsx
+++ client/src/WordPage.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { KeyboardEventHandler, useCallback } from "react";
+import { useState } from "react";
 
 const verbs = [
     "pleuvoir", 
@@ -22,3 +23,26 @@ const adjectives = [
     "raide", 
 
 ]
+
+export default function WordComponent() {
+    const [correctWord, setCorrectWord] = useState<string>("courage")
+    const [word, setWord] = useState<Array<String>>([])
+
+    const handleKeyboard: KeyboardEventHandler<HTMLDivElement> = useCallback((eventName: KeyboardEvent) => {
+        console.log(eventName.key)
+    }, [])
+
+
+    return (
+        <div className="m-10 flex" onKeyDown={handleKeyboard}>
+            {
+            correctWord.split("").map((letter, index) => {
+                return (
+                <div className="border-2 border-blue-800 w-10 h-10 m-1 text-center">{letter}</div>)
+            })
+            }
+        </div>
+        
+    )
+
+}
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import HomePage from './HomePage';
+import WordComponent from './WordPage';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <HomePage />
+    <WordComponent />
   </React.StrictMode>
 );
 
