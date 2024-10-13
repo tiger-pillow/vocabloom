19f8cdeb0ec1562ac139eb5447c5404c2a283786
yiyuan <gladysfangcs@gmail.com>
2024-10-13 14:23:10 +0200

    added landing page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import HomePage from './pages/HomePage';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AdminPage from './pages/AdminPages/AdminPage';
 import AddCardPage from './pages/AdminPages/AddCardPage';
+import WaitlistPage from './pages/WaitlistPage';
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
       <header className="App-header">
         <Router>
           <Routes>
-            <Route path="/" element={<HomePage />} />      {/* Home Route */}
+            <Route path = "/" element = {<WaitlistPage></WaitlistPage>}></Route>
+            <Route path="/cards" element={<HomePage />} />      {/* Home Route */}
             <Route path="/admin" element={<AdminPage />} /> {/* Admin Route */}
             <Route path = "/addcard" element = {<AddCardPage/>} />
           </Routes>
---
+++ client/src/pages/WaitlistPage.tsx
@@ -0,0 +1,40 @@
+
+import axiosConfig from "../axiosConfig";
+
+export default function WaitlistPage() {
+
+
+
+    return (
+        <div className="bg-gray-100 h-screen flex items-center justify-center">
+            <div className="text-center max-w-4xl mx-auto">
+                <h1 className="text-5xl sm:text-7xl lg:text-8xl font-extrabold text-gray-800 leading-tight tracking-tight">
+                    VocaBloom
+                </h1>
+
+                <p className="mt-6 text-lg sm:text-xl lg:text-2xl text-gray-500">
+                    Elevate your vocabulary, one word at a time.
+                </p>
+
+                <div className="mt-10">
+                    <form className="flex flex-col sm:flex-row justify-center items-center">
+                        <input
+                            type="email"
+                            placeholder="Enter your email"
+                            className="p-3 w-full sm:w-96 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-md"
+                        />
+                        <button
+                            type="submit"
+                            className="mt-4 sm:mt-0 sm:ml-4 bg-gray-500 text-white px-6 py-3 rounded-lg hover:bg-blue-600 text-md">
+                            Register
+                        </button>
+                    </form>
+                    <div className="flex justify-start text-gray-800 text-xs"> Expected spring 2025</div>
+                </div>
+            </div>
+
+            
+        </div>
+    )
+
+}
