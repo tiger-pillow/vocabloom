de65a51dde3c186372e16bb467acf0f0eddaaaf3
yiyuan <gladysfangcs@gmail.com>
2024-10-15 06:24:55 +0200

    fixing bash, removing the app

--- src/index.ts
+++ src/index.ts
@@ -50,7 +50,7 @@ if (process.env.NODE_ENV === 'production') {
     // Set static folder
     app.use(express.static('client/build'));
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve('app', 'client', 'build', 'index.html'));
+        res.sendFile(path.resolve('client', 'build', 'index.html'));
     });
 }
 
