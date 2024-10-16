88105a7f0660b7d15615df7ac2629adf9de04476
yiyuan <gladysfangcs@gmail.com>
2024-10-17 00:15:54 +0200

    changed tabname, changed favicon to logo

--- client/src/pages/WaitlistPage.tsx
+++ client/src/pages/WaitlistPage.tsx
@@ -9,24 +9,27 @@ export default function WaitlistPage() {
                     vocabloom
                 </h1>
 
-                <p className="mt-6 text-lg sm:text-xl lg:text-2xl text-gray-500">
-                    Elevate your vocabulary, one word at a time.
+                <p className="mt-6 text-lg sm:text-xs lg:text-2xl text-gray-500">
+                    Tackle foreign language, one word at a time.
                 </p>
+                
+                <div className="pl-1 mt-3 flex justify-center text-gray-500 text-xs">Expected early 2025</div>
 
                 <div className="mt-10">
                     <form className="flex flex-col sm:flex-row justify-center items-center">
                         <input
                             type="email"
                             placeholder="Enter your email"
-                            className="p-3 w-full sm:w-96 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-md"
+                            className="p-2 w-full sm:w-96 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-md"
                         />
                         <button
                             type="submit"
-                            className="mt-4 sm:mt-0 sm:ml-4 bg-gray-500 text-white px-6 py-3 rounded-lg hover:bg-blue-600 text-md">
+                            className="mt-4 sm:mt-0 sm:ml-4 bg-gray-500 text-white px-6 py-3 rounded-lg hover:bg-yellow-500 text-md">
                             Register
                         </button>
                     </form>
-                    <div className="flex justify-start text-gray-800 text-xs"> Expected spring 2025</div>
+                    
+
                 </div>
             </div>
 
